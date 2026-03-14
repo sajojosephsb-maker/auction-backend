8424fda482efd6eab04de5a4ed7a50cfddc0b3ae
sajojosephsb-maker <268094472+sajojosephsb-maker@users.noreply.github.com>
2026-03-14 14:47:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,6 @@
-// Add to your State Management
-let biddingOpen = false; 
-
-io.on('connection', (socket) => {
-    socket.on('adminAction', (data) => {
-        if (data.password !== ADMIN_PASSWORD) return;
-        
-        if (data.action === 'OPEN_BIDS') {
-            biddingOpen = true;
-            io.emit('statusUpdate', { message: "Bidding is now OPEN!" });
-        } else if (data.action === 'CLOSE_BIDS') {
-            biddingOpen = false;
-            io.emit('statusUpdate', { message: "Bidding is CLOSED." });
-        }
-    });
-
-    socket.on('placeBid', (data) => {
-        // The lock check
-        if (!biddingOpen) {
-            return socket.emit('error', "Bidding is currently locked by Admin.");
-        }
-        
-        if (!auctionState.isEnded && data.amount > auctionState.highestBid) {
-            auctionState.highestBid = data.amount;
-            auctionState.highestBidder = data.bidderName;
-            io.emit('updateBid', auctionState);
-        }
-    });
-});
+if (data.action === 'WITHDRAW_LOT') {
+    auctionState.isEnded = true;
+    auctionState.highestBidder = "WITHDRAWN";
+    io.emit('updateBid', auctionState);
+    io.emit('statusUpdate', { message: `Lot ${auctionState.currentLot} has been WITHDRAWN.` });
+}
