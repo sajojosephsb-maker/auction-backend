82c750f27ed91a0237d85904fda8058128813468
sajojosephsb-maker <268094472+sajojosephsb-maker@users.noreply.github.com>
2026-03-14 13:00:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,45 @@
-// Add this to your global variables
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http, { cors: { origin: "*" } });
+
+const ADMIN_PASSWORD = "spices_admin_2026";
 let participantCount = 0;
+let lotHistory = [];
+let pendingBuyers = [];
+let approvedBuyers = {}; 
+
+let auctionState = {
+    currentLot: "LOT-101: Bold Green Cardamom",
+    highestBid: 1200,
+    highestBidder: "No Bids",
+    timeLeft: 60,
+    isEnded: false,
+    specs: { totalWeight: 600 }
+};
 
 io.on('connection', (socket) => {
     participantCount++;
     io.emit('updateParticipantCount', participantCount);
-    console.log(`Participants: ${participantCount}`);
+    socket.emit('updateBid', auctionState);
+    socket.emit('updateLotHistory', lotHistory);
+
+    // MESSAGE BROADCAST
+    socket.on('adminBroadcast', (data) => {
+        if (data.password === ADMIN_PASSWORD) {
+            io.emit('showAnnouncement', data.message);
+        }
+    });
+
+    socket.on('registerBuyer', (data) => {
+        pendingBuyers.push({ id: socket.id, ...data });
+        io.emit('newRegistrationRequest', pendingBuyers);
+    });
 
     socket.on('disconnect', () => {
         participantCount--;
         io.emit('updateParticipantCount', participantCount);
     });
-    
-    // ... rest of your existing connection logic
 });
+
+http.listen(process.env.PORT || 10000, () => { console.log('Server Live'); });
